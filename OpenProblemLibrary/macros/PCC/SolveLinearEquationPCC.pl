0a2a42fcb310c5a4903e677cf6042484e7aaae40
Alex Jordan <alex.jordan@pcc.edu>
2018-05-04 15:49:26 -0700

    updates and additions to PCC macros

--- OpenProblemLibrary/macros/PCC/SolveLinearEquationPCC.pl
+++ OpenProblemLibrary/macros/PCC/SolveLinearEquationPCC.pl
@@ -1,5 +1,7 @@
 #These three subroutines uniformize how all our "solve this equation" problems are handled.
 
+loadMacros("PCCmacros.pl");
+
 sub _init_SolveLinearEquationPCCDevel {
 
  #Possibly add initialization code here
@@ -22,6 +24,8 @@ sub contextSetup {
   parser::Assignment->Allow;
   Context()->flags->set(reduceConstantFunctions=>0,
                         formatStudentAnswer=>"parsed",
+                        reduceConstants=>0,
+                        formatStudentAnswer=>parsed,
                         reduceFractions=>0,
                         showExtraParens=>0);
   Context()->operators->redefine(',');
@@ -71,33 +75,38 @@ sub instructions {
   };
   my @eqTypes = ();
   for my $i (keys %varAddHash) {
-    push (@eqTypes, '*'.$i.' = ___ *');
+    push (@eqTypes, '[|'.$i.' = ___|]*');
   };
   my $eqTypesString = join(', ',@eqTypes);
-  "Solve the following linear equation; the answer could be in the form $eqTypesString, *no solution*, or *all real numbers*."
+  #"Solve the following linear equation; the answer could be in the form $eqTypesString, [|no solution|]*, or [|all real numbers|]*."
+  'Solve the equation.'.KeyboardInstructions(q! (The answer might be "[|no solution|]*" or "[|all real numbers|]*".)!)
 };
 
 
 sub answerCheck {
   my ($ansEqRef, $eqTypesRef) = @_;
   for my $i (0..$#{$ansEqRef}) {
-    $ansType = $eqTypesRef->[$i];
-    $ansEq = $ansEqRef->[$i];
-    ANS($ansEq->cmp(
-      cmp_class=>"an equation of the form $ansType"
+    $ansType[$i] = $eqTypesRef->[$i];
+    $ansEq[$i] = $ansEqRef->[$i];
+    ANS($ansEq[$i]->cmp(
+      cmp_class=>"a variable assignment of the form $ansType[$i]",
+      correct_ans_latex_string => '\\{' . (Compute($ansEq[$i])->value)[1] . '\\}',
+      correct_ans => '{' . (Compute($ansEq[$i])->value)[1] . '}',
     )->withPostFilter(
       AnswerHints( 
-        (Compute($ansEq)->value)[1] => ["You have the solution, but the answer to this question should be in the form $ansType" , replaceMessage => 1],
-        sub {
-          my ($correct, $student, $ansHash) =@_;
-          return (Compute($ansHash->{original_student_ans})->class eq 'Set');
-        } => ["You are trying to give the solution set in set notation, but the answer to this question should be in the form $ansType" , replaceMessage => 1],
-        sub {
-          my ($correct, $student, $ansHash) =@_;
-          return (Compute($ansHash->{original_student_ans})->class eq 'Set' and $student == (Compute($ansEq)->value)[1]);
-        } => ["You have the solution set, but the answers to this question should be in the form $ansType" , replaceMessage => 1],
+        #(Compute($ansEq[$i])->value)[1] => ["You have the solution, but the answer to this question should be in the form $ansType[$i]" , replaceMessage => 1],
+        #sub {
+        #  my ($correct, $student, $ansHash) =@_;
+        #  return (Compute($ansHash->{original_student_ans})->class eq 'Set');
+        #} => ["You are trying to give the solution set in set notation, but the answer to this question should be in the form $ansType[$i]" , replaceMessage => 1],
+        #sub {
+        #  my ($correct, $student, $ansHash) =@_;
+        #  return (Compute($ansHash->{original_student_ans})->class eq 'Set' and $student == (Compute($ansEq[$i])->value)[1]);
+        #} => ["You have the solution set, but the answers to this question should be in the form $ansType[$i]" , replaceMessage => 1],
+        $ansEq[$i] => ["Good. And the solution set for this equation is {" . (Compute($ansEq[$i])->value)[1] . "}.", replaceMessage => 1, checkCorrect =>1],
       )
-    )->withPostFilter(sub {
+    )
+    ->withPostFilter(sub {
       my $ansHash = shift;
       if ($ansHash->{score}) {
         my ($svar,$sfrac) = $ansHash->{student_value}->value;
@@ -113,8 +122,27 @@ sub answerCheck {
             $ansHash->{score} = $check->{score};
             $ansHash->{ans_message} = $check->{ans_message};
           }
-        }
+       }
       }
+          #if we make it through all the pre-2017 answer checking, now check for answers like 12 or {12} (as opposed to x=12) and award credit
+          elsif (Value::classMatch($ansHash->{student_value},'Fraction') or Value::classMatch($ansHash->{student_value},'Real')) {
+            my $simpleans = Fraction((Compute($ansEq[$i])->value)[1]);
+            my $simplecheck = $simpleans->cmp->evaluate($ansHash->{student_value}->string);
+            $ansHash->{score} = $simplecheck->{score};
+            $ansHash->{ans_message} = $simplecheck->{ans_message};
+            if (Value::classMatch($ansHash->{student_value},'Fraction')) {
+              if (($ansHash->{student_value}->value)[1] == 1 or ($ansHash->{student_value}->value)[0] == 0) {
+                $ansHash->{score} = 0;
+                $ansHash->{ans_message} = "Your fraction is not reduced";
+              }
+            }
+          } 
+          elsif (Value::classMatch($ansHash->{student_value},'Set')) {
+            my $correctsol = (Compute($ansEq[$i])->value)[1];
+            my $check = $correctsol->cmp->evaluate(($ansHash->{student_value}->value)[0]->string);
+            $ansHash->{score} = $check->{score};
+            $ansHash->{ans_message} = $check->{ans_message};
+          } 
       return $ansHash;
     })
     );
@@ -129,8 +157,7 @@ sub summary {
   my $leftTeX = $left->TeX;
   my $rightTeX = $right->TeX;
   my $ansTeX = $ans->TeX;
-  "So the solution to this equation is that [`$ansEqTeX`]. We can also say that [`$var\\in\\left\\{$ansTeX\\right\\}`].  
-If we substitute [`$ansTeX`] in for [`$var`] in the original equation [`$leftTeX=$rightTeX`], the equation will be true. Please check this on your own - it is an important habit. "
+  "The solution to this equation is [`$ans`]. To stress that this is a value assigned to [`$var`], some report [`$ansEqTeX`]. We can also say that the solution set is [`\\{$ansTeX\\}`], or that [`$var\\in\\left\\{$ansTeX\\right\\}`]. If we substitute [`$ansTeX`] in for [`$var`] in the original equation [`$leftTeX=$rightTeX`], the equation will be true. Please check this on your own; it is an important habit. "
 
 };
 
