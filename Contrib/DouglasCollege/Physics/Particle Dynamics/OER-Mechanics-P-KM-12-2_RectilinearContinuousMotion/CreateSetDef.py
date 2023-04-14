7ec599c570fb394e78a23b36047189c1be6fdad6
Michael Sekatchev <michaelsekatchev@live.ca>
2023-04-14 13:05:43 -0700

    Adding OER Mechanics Project Contributions
    Updated the following contrib folders with problems from the OER Mechanics Project:
    DouglasCollege
    USask
    UBC/MECH/MECH2/Dynamics

---
+++ Contrib/DouglasCollege/Physics/Particle Dynamics/OER-Mechanics-P-KM-12-2_RectilinearContinuousMotion/CreateSetDef.py
@@ -0,0 +1,34 @@
+# %%
+
+#Description:
+# Creates the .def file for the problems in the current folder
+# Adds all files to a .tar.gz archive
+
+import os
+import fileinput
+import shutil
+
+cwd = os.getcwd()
+currentfolder = cwd.split("\\")[-1]
+directory = os.listdir(os.getcwd())
+
+FolderName = input("What be the folder name?")
+
+header = """setNumber=Chapter2_UBC_OER_Mechanics
+openDate = 1/7/00 at 6:00am
+dueDate = 1/20/09 at 6:00am
+answerDate = 1/21/09 at 6:00
+paperHeaderFile = defaultHeader
+screenHeaderFile = defaultHeader
+problemList =\n"""
+
+content = ""
+
+for filename in directory:
+    if filename.endswith('.pg'):
+        content += FolderName + '/' + filename + ', 1\n'
+
+content = header + content
+f = open('set_'+FolderName+'.def','w', encoding='utf-8')  #create new file
+f.write(content)              #write to fole
+f.close()
